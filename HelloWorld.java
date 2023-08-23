975b186595b41e87af3c46f1cc1f3d25d92754e7
yanqi666 <98233811+yanqi666@users.noreply.github.com>
2023-08-22 23:51:31 -0500

    Revert "change name to sunny liu"

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Sunny Liu.");
+        System.out.println("Hello, World! Yan Qi.");
     }
 }
