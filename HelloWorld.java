0b13fd044375a78d36f353069b99a06b484e31ea
Yan Qi <duo15@txstate.edu>
2023-08-22 23:41:03 -0500

    chenge name to house and delete Myclass

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Yan Qi.");
+        System.out.println("Hello, World! House.");
     }
 }
